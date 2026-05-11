e1120e38a58620b8701033e19df31d3e03d5176c
minusone <sgone2842@gmail.com>
2026-05-11 12:05:13 +0530

    refactor: implement cleanup logic upon exit

--- backend/src/simulation/seed_data.py
+++ backend/src/simulation/seed_data.py
@@ -7,15 +7,17 @@ from .db_utils import create_random_jobs
 
 async def data_inflow():
     # seed new data with intervals
-    for _ in range(10):
-        roll = random.randint(1, State.inflow_size)
-        new_jobs = create_random_jobs(roll)
-        async with State.lock:
-            State.queued += new_jobs
-            total_jobs = State.queued
-        print(f"News jobs arrived -> {new_jobs}, Total jobs -> {total_jobs}")
-        await asyncio.sleep(3)
-
-    # flag to mark end of incoming data    
-    State.data_flow = False
+    try:
+        for _ in range(10):
+            roll = random.randint(1, State.inflow_size)
+            new_jobs = create_random_jobs(roll)
+            async with State.lock:
+                State.queued += new_jobs
+                total_jobs = State.queued
+            print(f"News jobs arrived -> {new_jobs}, Total jobs -> {total_jobs}")
+            await asyncio.sleep(3)
+    finally:
+        # flag to mark end of incoming data   
+        async with State.lock: 
+            State.data_flow = False
     
