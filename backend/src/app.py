94b2e5ea4b254e79a06ebbef2609342d107c7a71
minusone <sgone2842@gmail.com>
2026-05-12 16:17:46 +0530

    feat: wire up frontend and config CORS

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
 from .api import register_routes
 from .database.engine import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
 from .database import models
+import os
+from dotenv import load_dotenv
+
 
 app = FastAPI()
+
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+if not FRONTEND_URL:
+    raise ValueError("FRONTEND_URL environment variable is not set")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=FRONTEND_URL,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
